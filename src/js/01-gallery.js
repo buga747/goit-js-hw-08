d69f9b72db9ab6cd324f9091a3fc0d7a00dfc993
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2022-11-28 16:46:41 +0200

    changes task 2 and 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryBox = document.querySelector('.gallery');
+const imagesMarkup = makeGalleryItemsMarkup(galleryItems);
+
+galleryBox.insertAdjacentHTML('beforeend', imagesMarkup);
+
+// creating gallery markup
+function makeGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" 
+  alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+// creating Lightbox
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionPosition: 'bottom',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const formEl = document.querySelector('.feedback-form');
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+initForm();
+
+function initForm() {
+  let savedData = localStorage.getItem(STORAGE_KEY);
+
+  if (savedData) {
+    savedData = JSON.parse(savedData);
+    Object.entries(savedData).forEach(([name, value]) => {
+      formData[name] = value;
+      formEl.elements[name].value = value;
+    });
+  }
+}
